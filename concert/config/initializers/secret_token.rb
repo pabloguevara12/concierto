973537dabfdeae67385098d604b4120bf78edf7d
pablo guevara <pabloguevara144@gmail.com>
2013-05-28 18:19:33 -0500

    concert

---
+++ concert/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Concert::Application.config.secret_token = '6756295028d8fbdd778d5a5498c3389a3b6609d0cbe9e1125e155591f87b4f284bc81cc9d628623aa6838cf4a195548eb8fb64c895e6f91a3eaab20c76b724a0'
---
+++ concert/db/schema.rb
@@ -0,0 +1,41 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20130528230635) do
+
+  create_table "roles", :force => true do |t|
+    t.integer  "rol_id"
+    t.string   "rol"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "type_users", :force => true do |t|
+    t.string   "nombre"
+    t.date     "finicio"
+    t.string   "correo"
+    t.string   "clave"
+    t.string   "rol_defecto"
+    t.integer  "tur_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "user_roles", :force => true do |t|
+    t.integer  "tur_id"
+    t.integer  "rol_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+end
