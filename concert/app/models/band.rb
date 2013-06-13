b8c79123b66399e19b375f1d6939d1e02390e280
emersoncardenas <retemk@gmail.com>
2013-06-12 22:34:04 -0500

    Rel12

--- concert/app/models/band.rb
+++ concert/app/models/band.rb
@@ -3,6 +3,9 @@ class Band < ActiveRecord::Base
   has_and_belongs_to_many :name_concert
   has_and_belongs_to_many :usuario
 
+validates_presence_of :name, :message => '^ Debes ingresar un nombre de Banda'
+validates_presence_of :member, :message => '^ Debes elegir un miembro'
+
 
   has_many :band_usuarios
   has_many :usuarios, :through => :band_usuarios
